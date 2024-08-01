f25d95b8520960e3ccf58cdec1b0f1f35ff1a4cb
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-08-01 15:01:49 +0530

    remove unnecessary background

--- Frontend/src/components/SidebarMenu.jsx
+++ Frontend/src/components/SidebarMenu.jsx
@@ -67,7 +67,7 @@ const SidebarMenu = () => {
         }`}
       >
         <div className="sm:p-4 p-2 flex flex-col justify-between h-[100vh]">
-          <div className="flex justify-between items-center bg-red-700 sm:h-[8vh] h-[7vh]">
+          <div className="flex justify-between items-center  sm:h-[8vh] h-[7vh]">
            
               <div className='sm:h-14 sm:w-14 h-12 w-12 rounded-full border-2  bg-slate-100' style={{borderColor:color}}>
               
@@ -81,7 +81,7 @@ const SidebarMenu = () => {
               onClick={toggleSidebar}
             />
           </div>
-          <div className="h-[80vh]  bg-green-700">
+          <div className="h-[80vh]  ">
             <div className="sm:font-bold font-medium md:hidden  ">
               <div class="border-b border-gray-300 p-2  hover:cursor-pointer">HOME</div>
               <div class="border-b border-gray-300 p-2  hover:cursor-pointer"
@@ -133,7 +133,7 @@ const SidebarMenu = () => {
 
             </div>
           </div>
-          <div className="h-[8vh]  bg-amber-300 flex justify-center gap-2 items-center font-bold hover:cursor-pointer text-red-600">
+          <div className="h-[8vh]   flex justify-center gap-2 items-center font-bold hover:cursor-pointer text-red-600">
             <div className="text-xl">Logout </div>
             <LogoutOutlinedIcon fontSize="large" />
           </div>
