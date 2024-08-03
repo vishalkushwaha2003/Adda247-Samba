6204ef2f116fe49d1b02e174060234bb8e01ae21
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-08-03 14:18:30 +0530

    complete styling part of the course component

--- Frontend/src/components/CourseComponent.jsx
+++ Frontend/src/components/CourseComponent.jsx
@@ -7,7 +7,7 @@ import Fade from '@mui/material/Fade';
 import IconButton from '@mui/material/IconButton';
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
 import Box from '@mui/material/Box';
-
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 const style = {
   position: 'absolute',
   top: '50%',
@@ -21,7 +21,7 @@ const style = {
 };
 
 function CourseComponent() {
-  const { color } = useTheme();
+  const { color,theme } = useTheme();
   const [openConfirmation, setOpenConfirmation] = useState(false);
   const [openReadMore, setOpenReadMore] = useState(false);
 
@@ -31,19 +31,27 @@ function CourseComponent() {
   const handleCloseReadMore = () => setOpenReadMore(false);
 
   return (
-    <div className='h-72 bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-300 border-[2px] rounded-lg grid grid-cols-1 grid-rows-12 shadow-lg dark:hover:shadow-slate-600 hover:shadow-slate-400 duration-200'  style={{ borderColor: color }}>
-      <div className='col-span-1 row-span-3  flex justify-center items-center lg:text-5xl font-bold md:text-4xl text-4xl'>SSC CGL</div>
+    <div className='h-72 bg-slate-100 dark:bg-slate-700/50 text-slate-900 dark:text-slate-300 border-[1px]  rounded-lg grid grid-cols-1 grid-rows-12 shadow-lg dark:hover:shadow-slate-600 hover:shadow-slate-400 duration-200'  style={{ borderColor: color }}>
+      <div className='col-span-1 row-span-3  flex justify-center items-center lg:text-5xl font-bold md:text-4xl text-4xl' style={{color:color}}>SSC CGL</div>
       <div className='col-span-1 row-span-7  relative text-wrap overflow-hidden sm:px-5 px-3'>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis consequatur sapiente atque voluptatum ut repellat quisquam cum est id sint magnam tenetur quas, cupiditate ex quibusdam officia nobis, accusamus rem!  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis consequatur sapiente atque voluptatum ut repellat quisquam cum est id sint magnam tenetur quas, cupiditate ex quibusdam officia nobis, accusamus rem!
         <IconButton 
-          style={{ position: 'absolute', bottom:0, right:10 ,width:'45px',backdropFilter:'blur(3px)', backgroundColor:'rgba(255,255,255,0.1)'}} 
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 10,
+            width: '45px',
+            backdropFilter: 'blur(1px)',
+            backgroundColor: theme === 'dark' ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)',
+          }}
+          
         
           onClick={handleOpenReadMore}>
-          <ReadMoreIcon  fontSize='medium' sx={{color:'white'}}/>
+          <ReadMoreIcon  fontSize='medium' sx={{color:color}}/>
         </IconButton>
       </div>
       <div className='col-span-1 row-span-2  flex justify-center items-center'>
-        <Button variant="outlined" onClick={handleOpenConfirmation} sx={{color:color,borderColor:color,fontWeight:'500','&:hover':{borderColor:color}}}>Primary</Button>
+        <Button variant="text" onClick={handleOpenConfirmation} sx={{color:color,borderColor:color,fontWeight:'600','&:hover':{borderColor:color}}} >JOIN <AddOutlinedIcon fontSize='small'/> </Button>
       </div>
       
       <Modal
