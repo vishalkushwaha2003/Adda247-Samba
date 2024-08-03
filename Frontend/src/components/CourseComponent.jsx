6adbe7d032dd77167ebddad9261d2b35acb4512c
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-08-03 11:21:53 +0530

    add textsize and font

--- Frontend/src/components/CourseComponent.jsx
+++ Frontend/src/components/CourseComponent.jsx
@@ -7,7 +7,7 @@ function CourseComponent() {
 
   return (
     <div className='h-72  bg-slate-500 border-2 rounded-lg   grid grid-cols-1 grid-rows-6' style={{borderColor:color}}>
-        <div className='col-span-1 row-span-2 bg-stone-900'>SSC CGL</div>
+        <div className='col-span-1 row-span-2 bg-slate-500 flex justify-center items-center lg:text-5xl font-bold md:text-4xl text-4xl  '>SSC CGL</div>
         <div className='col-span-1 row-span-3 bg-red-700'>para</div>
         <div className='col-span-1 row-span-1 bg-yellow-500'></div>
     </div>
