16adc321d914dacf52be4e7eb0372723511709dd
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-08-05 00:45:10 +0530

    adjust the padding

--- Frontend/src/components/CourseComponent.jsx
+++ Frontend/src/components/CourseComponent.jsx
@@ -102,7 +102,7 @@ function CourseComponent({name, fullform, description, preparationGuide,courseNa
         disableScrollLock={true}
       >
         <Fade in={openConfirmation}>
-          <Box className='absolute h-[100vh] w-[100vw] overflow-y-scroll pt-[40vh] ' >
+          <Box className='absolute h-[100vh] w-[100vw] overflow-y-scroll pt-[30vh]  ' >
             <div
             className=' sm:w-96 m-auto w-[80vw] bg-slate-100/80 dark:bg-slate-800/80 text-slate-900 dark:text-slate-200 border-[1px] rounded-lg flex flex-col gap-3 py-5 sm:px-5 px-3 shadow-lg   duration-200' style={{ borderColor: color }}
             
@@ -111,7 +111,7 @@ function CourseComponent({name, fullform, description, preparationGuide,courseNa
             <div className='m-auto lg:text-lg font-bold text-md ' >Confirmation</div>
             <p >Are you sure you want to proceed?</p>
             <div className='flex gap-2' >
-              {isChecked?<CheckBoxOutlinedIcon onClick={()=>setIsChecked(false)} sx={{ color: color, borderColor: color, fontWeight: '5d00', '&:hover': { borderColor: color } }}/>:<CheckBoxOutlineBlankOutlinedIcon fontSize='medium' onClick={()=>setIsChecked(true)}  sx={{ color: theme==='dark'?'rgb(203,213,225)':'rgb(15,23,42)', borderColor:theme==='dark'?'rgb(203,213,225)':'rgb(15,23,42)', fontWeight: '400', '&:hover': { borderColor: theme==='dark'?'rgb(203,213,225)':'rgb(15,23,42)' } }}/>}
+              {isChecked?<CheckBoxOutlinedIcon onClick={()=>setIsChecked(false)} sx={{ color: color, borderColor: color, fontWeight: '500', '&:hover': { borderColor: color } }}/>:<CheckBoxOutlineBlankOutlinedIcon fontSize='medium' onClick={()=>setIsChecked(true)}  sx={{ color: theme==='dark'?'rgb(203,213,225)':'rgb(15,23,42)', borderColor:theme==='dark'?'rgb(203,213,225)':'rgb(15,23,42)', fontWeight: '400', '&:hover': { borderColor: theme==='dark'?'rgb(203,213,225)':'rgb(15,23,42)' } }}/>}
               <p>Term and condition</p>
             </div>
             <div className='flex justify-around items-center'>
