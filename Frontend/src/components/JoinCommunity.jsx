6260ec1fb48795d2ddc1efa7faeeef85812baf32
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-08-05 03:07:22 +0530

    add the background to the join community

--- Frontend/src/components/JoinCommunity.jsx
+++ Frontend/src/components/JoinCommunity.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useTheme } from '../context/ColorContext'; // Ensure you import the useTheme hook
 
 function JoinCommunity() {
+  const { color } = useTheme(); // Destructure color from the context
+
   return (
-    <div>
-      JoinCommunity
+    <div
+      className="h-screen w-screen"
+      style={{
+        backgroundImage: `repeating-radial-gradient(rgb(51 65 85) 88%, ${color} 90%)`,
+        backgroundSize: '50px 50px',
+      }}
+    >
+      hii
     </div>
-  )
+  );
 }
 
-export default JoinCommunity
+export default JoinCommunity;
