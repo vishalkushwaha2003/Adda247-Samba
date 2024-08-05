eda1f34b4f5bdf515fecfb112917db6eda64cf04
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-08-05 14:47:43 +0530

    design social media card

--- Frontend/src/components/JoinCommunity.jsx
+++ Frontend/src/components/JoinCommunity.jsx
@@ -1,67 +1,14 @@
-import React from 'react';
-import {useTheme} from'../context/ColorContext'
+import React from 'react'
+import SocialMediaCard from './SocialMediaCard'
 
-const CustomBackground = () => {
-  const { color } = useTheme();
-  const containerStyle = {
-    width: '100%',
-    height: '100%',
-    background: `
-      radial-gradient(
-          circle farthest-side at 0% 50%,
-          #282828 23.5%,
-          rgba(255, 170, 0, 0) 0
-        )
-        21px 30px,
-      radial-gradient(
-          circle farthest-side at 0% 50%,
-          #a1000e 24%,
-          rgba(240, 166, 17, 0) 0
-        )
-        19px 30px,
-      linear-gradient(
-          #282828 14%,
-          rgba(240, 166, 17, 0) 0,
-          rgba(240, 166, 17, 0) 85%,
-          #282828 0
-        )
-        0 0,
-      linear-gradient(
-          150deg,
-          #282828 24%,
-          #a1000e 0,
-          #a1000e 26%,
-          rgba(240, 166, 17, 0) 0,
-          rgba(240, 166, 17, 0) 74%,
-          #a1000e 0,
-          #a1000e 76%,
-          #282828 0
-        )
-        0 0,
-      linear-gradient(
-          30deg,
-          #282828 24%,
-          #a1000e 0,
-          #a1000e 26%,
-          rgba(240, 166, 17, 0) 0,
-          rgba(240, 166, 17, 0) 74%,
-          #a1000e 0,
-          #a1000e 76%,
-          #282828 0
-        )
-        0 0,
-      linear-gradient(90deg, #a1000e 2%, #282828 0, #282828 98%, #a1000e 0%) 0 0
-        #282828`,
-    backgroundSize: '40px 60px',
-  };
-
-  return <div style={containerStyle} >
-
-
-              <div className='h-screen w-screen'>
-
-              </div>
-  </div>;
+function JoinCommunity() {
+  return (
+    <div className='w-[80vw]  grid md:grid-cols-3 md:grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 md:gap-8 gap-5 grid-cols-1 grid-rows-3 m-auto pt-[20vh] '>
+      <SocialMediaCard/>
+      <SocialMediaCard/>
+      <SocialMediaCard/>
+    </div>
+  ) 
 }
-
-export default CustomBackground;
+ 
+export default JoinCommunity
---
+++ Frontend/src/components/SocialMediaCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SocialMediaCard() {
+  return (
+    <div className='h-60 bg-slate-200 dark:bg-slate-700 text-slate-900 dark:text-slate-200 rounded-lg duration-200 hover:shadow-lg dark:hover:shadow-slate-950 hover:shadow-slate-400 '>
+      hii
+    </div>
+  )
+}
+
+export default SocialMediaCard
