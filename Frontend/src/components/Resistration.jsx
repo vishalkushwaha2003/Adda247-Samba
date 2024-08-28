68026a5a562dfedae5fab470e4b14c22ec3b390b
vishalmapsense <146525125+vishalkushwaha2003@users.noreply.github.com>
2024-08-29 04:54:12 +0530

    design the status of the sucess and store the token

--- Frontend/src/components/Resistration.jsx
+++ Frontend/src/components/Resistration.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import { useTheme } from "../context/ColorContext";
 import Button from "@mui/material/Button";
 import Backdrop from "@mui/material/Backdrop";
@@ -8,6 +8,9 @@ import Box from "@mui/material/Box";
 import CloseIcon from "@mui/icons-material/Close";
 import CheckBoxOutlinedIcon from "@mui/icons-material/CheckBoxOutlined";
 import CheckBoxOutlineBlankOutlinedIcon from "@mui/icons-material/CheckBoxOutlineBlankOutlined";
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+
+import 'ldrs/grid';
 
 const Registration = () => {
   const { theme, toggleTheme, color, changeColor } = useTheme();
@@ -15,6 +18,15 @@ const Registration = () => {
   const [fileUploadError,setFileUploadError]=useState(null);
   const [openPreview, setOpenPreview] = useState(false);
   const [previewUrl, setPreviewUrl] = useState(null);
+  const [showLoader,setShowLoader]=useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [successAnimation,setSuccessAnimation] = useState(false);
+  const [token,setToken]=useState(null)
+
+            
+ 
+
+  console.log('tocken form localStorage',token);
 
   const [formData,setFormData]=useState({
     firstName: "",
@@ -137,6 +149,17 @@ const Registration = () => {
 
 
 
+
+
+  useEffect(() => {
+
+       setToken(localStorage.getItem("authToken"));
+
+  },[token]);
+  
+  
+
+
   const handleTicked = () => {
     setIsTicked(!isTicked);
   };
@@ -149,13 +172,19 @@ const Registration = () => {
 
   }
 
+
   const formHandler = (e) => {
     e.preventDefault();
     setOpenPreview(true);
+    setShowLoader(true); // Show the loader
 
-  
-   
-  };
+    // Set a timeout to hide the loader after 3 seconds
+    setTimeout(() => {
+        setShowLoader(false);
+        
+    }, 2000); // 3000 milliseconds = 3 seconds
+};
+
   
 
 
@@ -170,6 +199,7 @@ const Registration = () => {
 
 
  const sendDataToBackendHandler =async()=>{
+          setShowLoader(true);
 
   try {
     const response = await fetch("http://localhost:3000/auth/register", {
@@ -183,83 +213,101 @@ const Registration = () => {
     if (!response.ok) {
       throw new Error("Failed to send data to the server");
     }
+    
+   
+
 
     const data = await response.json(); 
     console.log("Response from server:", data);
+
+    if (data && data.token) {
+      // Store the token in localStorage
+     const token = localStorage.setItem("authToken", data.token);
+      setShowLoader(false);
+      setSuccessAnimation(true);
+      setToken(token);
+    }
   } catch (error) {
     console.error("Error:", error);
   }
  }
 
-
+const removeToken=()=>{
+  localStorage.removeItem("authToken");
+  setToken(null);
+  
+}
 
 
   return (
     <div>
-    <form
-      className="max-w-fit flex flex-col gap-4 m-auto  sm:p-6 p-4 border border-slate-500 rounded-2xl dark:bg-slate-800 bg-slate-200"
-      onSubmit={formHandler}
-    >
-      <p
-        className="text-2xl font-semibold tracking-tight m-auto"
-        style={{ color: color }}
-      >
-        Registration Form
-      </p>
-      <p className="text-base text-slate-500 dark:text-slate-400">
-        Signup now and start learning with our expert faculty.
-      </p>
-      <div className="w-[90vw] grid sm:grid-cols-2 sm:grid-rows-6 sm:gap-5 gap-3">
-        {formFields.map((data, key) => {
-          const field = data;
-
-          return (
-            <label id={key} className="relative">
-              <input
-                id={field.id}
-                name={field.name}
-                className="input dark:bg-slate-700 bg-slate-300 dark:text-slate-100 text-slate-950 w-full sm:px-4 sm:py-3 px-3 py-2 outline-none border border-slate-500 rounded-lg"
-                type={field.type}
-                placeholder={field.placeholder}
-                required={field.required}
-                onChange={handleChange}
-                value={formData[field.name]}
-                
-              />
-            </label>
-          );
-        })}
-      </div>
-
-      <Button
-        variant="outlined"
-        sx={{
-          width: "400px",
-          margin: "auto",
-          marginTop: "20px",
-          fontWeight: "bold",
-          "@media (max-width: 768px)": { width: "300px" }, // Medium screens
-          "@media (max-width: 640px)": { width: "200px" }, // Small screens
-          color: color,
-          borderColor: color,
-          "&:hover": { borderColor: color },
-        }}
-        type="submit"
-      >
-        Submit
-      </Button>
-      <p className="text-center dark:text-slate-400 text-slate-500">
-        Already have an account?{" "}
-        <a
-          href="#"
-          className="underline mx-2 font-medium"
-          style={{ color: color }}
+      {!token ? (
+        <form
+          className="max-w-fit flex flex-col gap-4 m-auto  sm:p-6 p-4 border border-slate-500 rounded-2xl dark:bg-slate-800 bg-slate-200"
+          onSubmit={formHandler}
         >
-          Signin
-        </a>
-      </p>
-    </form>
-    <Modal
+          <p
+            className="text-2xl font-semibold tracking-tight m-auto"
+            style={{ color: color }}
+          >
+            Registration Form
+          </p>
+          <p className="text-base text-slate-500 dark:text-slate-400">
+            Signup now and start learning with our expert faculty.
+          </p>
+          <div className="w-[90vw] grid sm:grid-cols-2 sm:grid-rows-6 sm:gap-5 gap-3">
+            {formFields.map((data, key) => {
+              const field = data;
+
+              return (
+                <label id={key} className="relative">
+                  <input
+                    id={field.id}
+                    name={field.name}
+                    className="input dark:bg-slate-700 bg-slate-300 dark:text-slate-100 text-slate-950 w-full sm:px-4 sm:py-3 px-3 py-2 outline-none border border-slate-500 rounded-lg"
+                    type={field.type}
+                    placeholder={field.placeholder}
+                    required={field.required}
+                    onChange={handleChange}
+                    value={formData[field.name]}
+                  />
+                </label>
+              );
+            })}
+          </div>
+
+          <Button
+            variant="outlined"
+            sx={{
+              width: "400px",
+              margin: "auto",
+              marginTop: "20px",
+              fontWeight: "bold",
+              "@media (max-width: 768px)": { width: "300px" }, // Medium screens
+              "@media (max-width: 640px)": { width: "200px" }, // Small screens
+              color: color,
+              borderColor: color,
+              "&:hover": { borderColor: color },
+            }}
+            type="submit"
+          >
+            Submit
+          </Button>
+          <p className="text-center dark:text-slate-400 text-slate-500">
+            Already have an account?{" "}
+            <a
+              href="#"
+              className="underline mx-2 font-medium"
+              style={{ color: color }}
+            >
+              Signin
+            </a>
+          </p>
+        </form>
+      ) : (
+        <div >there is ui after the successful form submition <Button onClick={removeToken} className="">remov token</Button> </div>
+      )}
+      <Modal
         open={openPreview}
         onClose={handleClosePreview}
         closeAfterTransition
@@ -275,85 +323,112 @@ const Registration = () => {
       >
         <Fade in={openPreview}>
           <Box className="absolute h-[100vh] w-[100vw] overflow-y-scroll md:py-20 py-16 ">
-            <div
-              className="lg:w-[80vw] w-[90vw] m-auto bg-slate-100/80 dark:bg-slate-800/80 text-slate-900 dark:text-slate-200 border-[1px] rounded-lg py-8 lg:px-12 sm:px-8 msm:px-4 shadow-lg dark:hover:shadow-slate-600 hover:shadow-slate-400 duration-200 relative "
-              style={{ borderColor: color }}
-            >
+            {token?<div className=" sm:h-60 sm:max-w-72 max-w-60 h-40 mx-auto mt-40 rounded-md relative flex flex-col justify-center items-center sm:gap-5 border bg-[#09090943] dark:bg-[#bdbdbd48] animate__animated animate__bounceIn" style={{borderColor:color}}>
+                  
+
               <div className="absolute top-1 right-1 hover:cursor-pointer">
-                <Button sx={{ color: "red" }} onClick={handleClosePreview}>
-                  <CloseIcon />
-                </Button>
-              </div>
+                  <Button sx={{ color: "red" }} onClick={handleClosePreview}>
+                    <CloseIcon />
+                  </Button>
+                </div>
+              
+                 
+                  <CheckCircleOutlinedIcon  sx={{width:'100px',height:'100px',color:color}}  />
+                  <div className="sm:text-2xl text-lg font-bold hover:cursor-default " style={{color:color}}>Succefully Submited</div>
+                  
 
-              <div className="lg:text-3xl md:text-2xl text-xl font-bold text-center ">
-                Form Preview
+            </div>: showLoader ? (
+              <div className=" h-full flex justify-center items-center">
+                <l-grid size="150" speed="1.5" color={color}></l-grid>
               </div>
-              <div className="grid grid-cols-1 py-8  md:grid-cols-2 gap-5  ">
-                {Object.entries(formData).map(([key, value], id) => {
-                  if (key !== "studentPhoto")
-                    return (
-                      <div
-                        key={id}
-                        className="col-span-1  text-wrap break-words  "
-                      >
-                        <label className="text-md px-1 font-normal text-slate-700 dark:text-slate-300 duration-200 text-nowrap">
-                          {previewLabel[id]}
-                        </label>
-                        :
-                        <span className="px-1  transition-all text-lg font-medium text-slate-700 dark:text-slate-300 duration-200 ">
-                          {value}
-                        </span>
-                      </div>
-                    );
-                })}
+            ) : (
+              <div
+                className="lg:w-[80vw] w-[90vw] m-auto bg-slate-100/80 dark:bg-slate-800/80 text-slate-900 dark:text-slate-200 border-[1px] rounded-lg py-8 lg:px-12 sm:px-8 msm:px-4 shadow-lg dark:hover:shadow-slate-600 hover:shadow-slate-400 duration-200 relative "
+                style={{ borderColor: color }}
+              >
+                <div className="absolute top-1 right-1 hover:cursor-pointer">
+                  <Button sx={{ color: "red" }} onClick={handleClosePreview}>
+                    <CloseIcon />
+                  </Button>
+                </div>
 
-                {/* {formData.studentPhoto && (
-                  <div className="col-span-1 ">
-                    <h3 className="textmd font-semibold">Student Photo</h3>
-                    <img
-                      src={URL.createObjectURL(formData.studentPhoto)}
-                      alt="Student Photo"
-                      className="lg:h-96 md:h-80 sm:h-72 msm:h-60 object-contain rounded-lg shadow-md shadow-slate-950/50"
-                    />
-                  </div>
-                )} */}
-              </div>
+                <div className="lg:text-3xl md:text-2xl text-xl font-bold text-center ">
+                  Form Preview
+                </div>
+                <div className="grid grid-cols-1 py-8  md:grid-cols-2 gap-5  ">
+                  {Object.entries(formData).map(([key, value], id) => {
+                    if (key !== "studentPhoto")
+                      return (
+                        <div
+                          key={id}
+                          className="col-span-1  text-wrap break-words  "
+                        >
+                          <label className="text-md px-1 font-normal text-slate-700 dark:text-slate-300 duration-200 text-nowrap">
+                            {previewLabel[id]}
+                          </label>
+                          :
+                          <span className="px-1  transition-all text-lg font-medium text-slate-700 dark:text-slate-300 duration-200 ">
+                            {value}
+                          </span>
+                        </div>
+                      );
+                  })}
 
-              <div className="flex gap-1">
-                <div className="hover:cursor-pointer">
-                  {" "}
-                  {isTicked ? (
-                    <CheckBoxOutlinedIcon
-                      style={{ color: color }}
-                      onClick={handleTicked}
-                    />
-                  ) : (
-                    <CheckBoxOutlineBlankOutlinedIcon onClick={handleTicked} />
+                  {formData.studentPhoto && (
+                    <div className="col-span-1 ">
+                      <h3 className="textmd font-semibold">Student Photo</h3>
+                      <img
+                        src={URL.createObjectURL(formData.studentPhoto)}
+                        alt="Student Photo"
+                        className="lg:h-96 md:h-80 sm:h-72 msm:h-60 object-contain rounded-lg shadow-md shadow-slate-950/50"
+                      />
+                    </div>
                   )}
                 </div>
-                <p>Ensure that carefuly preview All field</p>
+
+                <div className="flex gap-1">
+                  <div className="hover:cursor-pointer">
+                    {" "}
+                    {isTicked ? (
+                      <CheckBoxOutlinedIcon
+                        style={{ color: color }}
+                        onClick={handleTicked}
+                      />
+                    ) : (
+                      <CheckBoxOutlineBlankOutlinedIcon
+                        onClick={handleTicked}
+                      />
+                    )}
+                  </div>
+                  <p>Ensure that carefuly preview All field</p>
+                </div>
+                <div className=" w-full flex justify-evenly  items-center h-20">
+                  <Button
+                    variant="outlined"
+                    sx={{
+                      color: "red",
+                      borderColor: "red",
+                      "&:hover": { borderColor: "red" },
+                    }}
+                    onClick={handleClosePreview}
+                  >
+                    Cancel
+                  </Button>{" "}
+                  {isTicked ? (
+                    <Button
+                      variant="contained"
+                      onClick={sendDataToBackendHandler}
+                    >
+                      submit
+                    </Button>
+                  ) : (
+                    <Button variant="contained" disabled>
+                      Submit
+                    </Button>
+                  )}
+                </div>
               </div>
-              <div className=" w-full flex justify-evenly  items-center h-20">
-                <Button
-                  variant="outlined"
-                  sx={{
-                    color: "red",
-                    borderColor: "red",
-                    "&:hover": { borderColor: "red" },
-                  }}
-                  onClick={handleClosePreview}
-                >
-                  Cancel
-                </Button>{" "}
-                {isTicked ? (
-                  <Button variant="contained" onClick={sendDataToBackendHandler}>submit</Button>
-                ) : (
-                  <Button variant="contained" disabled>
-                    Submit
-                  </Button>
-                )}
-              </div>
-            </div>
+            )}
           </Box>
         </Fade>
       </Modal>
